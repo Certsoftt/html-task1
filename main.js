13bdfef66b30f77e2dd0a0befcdb04b78e79df0f
Certsoftt <91556576+Certsoftt@users.noreply.github.com>
2024-08-09 14:57:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// CRUD Operation
+
+//Selecting or Reading from the DOM - querySelector()
+let head = document.querySelector("head");
+
+//Create for the DOM - createElement()
+let p = document.createElement("p");
+
+p.textContent = "First Paragraph";
+
+//insertBefore(), appendChild(), prependChild()
+
+head.insertBefore(2, p);
+
+
+//Deleteing from the DOM - remove(), removeChild()
+
+
+//Updating the DOM - replaceChild()
+
+
+//create an anchor element tag, store it inside the body
+//add some text to the anchor element tag
+//make the text clickable. Hint: add the link google.com to the text
